4e34f1bff286bbea0385c827b76284d4695968f5
Mario Sanchez Lara <mario@mariosl.com>
2025-03-20 18:16:29 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Homebrew taps for usage with nix-homebrew.";
+
+  inputs = {
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+  }
+
+  outputs = inputs@{ self, homebrew-core, homebrew-cask }: {
+    taps = {
+      "homebrew/homebrew-core" = homebrew-core;
+      "homebrew/homebrew-cask" = homebrew-cask;
+    };
+  }
+}
